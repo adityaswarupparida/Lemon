78bfcb8f906e4dcf3e6105d340448c3975b796cc
aparida <asparida33@gmail.com>
2025-11-29 01:31:10 +0530

    add code parser

--- apps/web/app/components/messageBubble.tsx
+++ apps/web/app/components/messageBubble.tsx
@@ -19,10 +19,43 @@ export const MessageBubble = ({ message, loading } : { message: Message, loading
             {message.role == `assistant` && (
                 <div className={`flex justify-start`}>
                     <div className="prose">
-                        <ReactMarkdown>{message.content}</ReactMarkdown>
+                        <ReactMarkdown>{parseMessage(message.content)}</ReactMarkdown>
                     </div>
                 </div>
             )}  
         </div>
     );
 }
+
+const parseMessage = (message: string) => {
+    let str = "";
+    let index = 0;
+    
+    while (true) {
+        const startOpenTag = message.indexOf("<artifact", index);
+        if (startOpenTag == -1) { 
+            str += message.slice(index);
+            break;
+        }
+        if (index < startOpenTag) {
+            str += message.slice(index, startOpenTag);
+        }
+        const endOpenTag = message.indexOf(">", startOpenTag);
+        if (endOpenTag == -1) {
+            break;
+        }
+
+        const startCloseTag = message.indexOf("</artifact", endOpenTag);
+        if (startCloseTag == -1) {
+            break;
+        }
+        str += message.slice(endOpenTag, startCloseTag);
+
+        const endCloseTag = message.indexOf(">", startCloseTag);
+        if (endCloseTag == -1) {
+            break;
+        }
+        index = endCloseTag;
+    }
+    return str;
+} 
